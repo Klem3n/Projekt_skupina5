7e5a3fd4bc0c2eb4ee9b4f9a5518963000ccbdda
Matic <matic18@protonmail.com>
2020-05-23 21:14:22 +0200

    site structure

---
+++ client/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Navbar />
+    <h1>Home Vue.js</h1>
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar";
+
+export default {
+  name: "Home",
+  components: {
+    Navbar
+  }
+};
+</script>
+
+<style>
+</style>
