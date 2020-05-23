3bc677f29aeed56dda9e071bb5a425e4145b0bc3
Matic <matic18@protonmail.com>
2020-05-23 19:04:05 +0200

    cleanup

--- server/api_endpoints.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-// Test Endpoint
-router.get('/test', (req, res) => {
-    res.send(JSON.stringify("Server seems to be working."))
-})
-
-module.exports = router
