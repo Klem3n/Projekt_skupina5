0de9d8da63696d9a8c30114b1f6d04e9a4afaa1a
Matic <matic18@protonmail.com>
2020-05-25 21:57:00 +0200

    🇸🇮

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
-        <h1 class="title is-5">Prometna infrastruktura</h1>
+      <a class="navbar-item" href="#">
+        <h1 class="title is-5">Prometna infrastruktura RS 🇸🇮</h1>
       </a>
       <a
         role="button"
