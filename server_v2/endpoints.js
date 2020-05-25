860328cde88befccdce3b5941b865b8f56036799
Klemen <klem3n.development@outlook.com>
2020-05-25 20:37:50 +0200

    Start of average speed

--- server_v2/endpoints.js
+++ server_v2/endpoints.js
@@ -2,10 +2,16 @@ const express = require('express')
 
 const Scraper = require('./scraper')
 
+const PovpHitrost = require('./povp_hitrost')
+
 const router = express.Router()
 
 router.get('/api/v1/stanje', async (req, res) => {
     res.send(await Scraper.runScraper());
+})
+
+router.get('/api/v1/povprecna_hitrost/:cesta', async (req, res) => {
+    res.send(await PovpHitrost.run());
 })
 
 module.exports = router
