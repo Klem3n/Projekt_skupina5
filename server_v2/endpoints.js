c70d5a81eace4f12108b8f33f467da448e4dbc2b
Matic <matic18@protonmail.com>
2020-05-25 20:40:36 +0200

    response is now valid json

--- server_v2/endpoints.js
+++ server_v2/endpoints.js
@@ -5,7 +5,7 @@ const Scraper = require('./scraper')
 const router = express.Router()
 
 router.get('/api/v1/all', async (req, res) => {
-    res.send(await Scraper.runScraper());
+    res.send(JSON.stringify(await Scraper.runScraper()));
 })
 
 module.exports = router
