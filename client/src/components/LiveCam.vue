93d0c2befa37ab879084751b4de8c745dcb55ee8
Matic <matic18@protonmail.com>
2020-05-26 03:39:13 +0200

    finished livecam view

--- client/src/components/LiveCam.vue
+++ client/src/components/LiveCam.vue
@@ -3,9 +3,26 @@
     <Navbar />
     <section class="hero is-link is-fullheight-with-navbar">
       <div class="hero-body">
+        <!--
         <div v-if="isDone" class="container imagesContainer">
           <div v-for="item in imagesArray" :key="item.url">
             <img :src="item" style="padding: 10px;" />
+          </div>
+        </div>
+        -->
+        <div v-if="isDone" style="margin-left: 35%; margin-right: 30%;">
+          <div
+            class="card"
+            v-for="item in testniArray"
+            :key="item.url"
+            style="margin-bottom: 30px; width: 28vw;"
+          >
+            <div class="card-content" style="display: flex;">{{ item.lokacija }}</div>
+            <div class="card-image">
+              <figure class="image is-4by5">
+                <img :src="item.url" />
+              </figure>
+            </div>
           </div>
         </div>
       </div>
@@ -29,6 +46,7 @@ export default {
     return {
       locationArray: [],
       imagesArray: [],
+      testniArray: [],
       isDone: false
     };
   },
@@ -37,6 +55,11 @@ export default {
     axios.get("http://localhost:5000/api/v1/kamere").then(response => {
       console.log(response);
       for (var i = 0; i < 138; i++) {
+        let obj = {
+          lokacija: response.data[i].name,
+          url: response.data[i].url
+        };
+        this.testniArray[i] = obj;
         this.locationArray[i] = response.data[i].name;
         this.imagesArray[i] = response.data[i].url;
       }
