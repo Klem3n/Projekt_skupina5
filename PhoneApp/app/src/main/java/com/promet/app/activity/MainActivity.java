086f819fb3610eabe4a1bb43e73f9325db4f4f20
MrsSeeker <goran.kirov@hotmail.com>
2020-05-31 14:02:31 +0200

    Senszor(Ziroskop)

--- PhoneApp/app/src/main/java/com/promet/app/activity/MainActivity.java
+++ PhoneApp/app/src/main/java/com/promet/app/activity/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public static final int PRIORITY_LOCATION_ACCURACY = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY;
     private static final int PERMISSIONS_FINE_LOCATION = 23;
     public static final float GRAVITY_EARTH = SensorManager.GRAVITY_EARTH;
+    public static final int SENSOR_DELAY_NORMAL = SensorManager.SENSOR_DELAY_NORMAL;
 
     private TextView tv_latiency;
     private TextView tv_longitude;
@@ -63,12 +64,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     Geocoder geocoder;
 
-    //Accelerometer
-    private TextView tv_X;
-    private TextView tv_Y;
-    private TextView tv_Z;
-    private Sensor sensor;
+    //Sensors
     private SensorManager sensorManager;
+    //Accelerometer
+    private TextView tv_accX;
+    private TextView tv_accY;
+    private TextView tv_accZ;
+    private Sensor accSensor;
+    //Gyroscope
+    private TextView tv_gyrX;
+    private TextView tv_gyrY;
+    private TextView tv_gyrZ;
+    private Sensor gyrSensor;
 
     //OPENCV
     static {
@@ -144,14 +151,32 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         updateGPS();
 
-        //Accelerometer
+        //Sensors
         sensorManager= (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor= sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(this, sensor, (int) GRAVITY_EARTH);
+        //Accelerometer
+        accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if(accSensor != null){
+            sensorManager.registerListener(MainActivity.this, accSensor, (int) GRAVITY_EARTH);
+        }
+        else {
+            tv_accX.setText("Listener not registered");
+        }
+        tv_accX = (TextView)findViewById(R.id.tv_accx);
+        tv_accY = (TextView)findViewById(R.id.tv_accy);
+        tv_accZ = (TextView)findViewById(R.id.tv_accz);
 
-        tv_X= (TextView)findViewById(R.id.tv_x);
-        tv_Y= (TextView)findViewById(R.id.tv_y);
-        tv_Z= (TextView)findViewById(R.id.tv_z);
+        //Gyroscope
+        tv_gyrX = (TextView)findViewById(R.id.tv_gyrx);
+        tv_gyrY = (TextView)findViewById(R.id.tv_gyry);
+        tv_gyrZ = (TextView)findViewById(R.id.tv_gyrz);
+        gyrSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        if(gyrSensor != null){
+            sensorManager.registerListener(MainActivity.this, gyrSensor, SENSOR_DELAY_NORMAL);
+        }
+        else {
+            tv_gyrX.setText("Listener not registered");
+        }
+
 
 
         //CAMERA
@@ -252,9 +277,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     //Accelerometer Methods
     @Override
     public void onSensorChanged(SensorEvent event) {
-        tv_X.setText(String.valueOf(event.values[0]));
-        tv_Y.setText(String.valueOf(event.values[1]));
-        tv_Z.setText(String.valueOf(event.values[2]));
+        Sensor sensor = event.sensor;
+        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            tv_accX.setText(String.valueOf(event.values[0]));
+            tv_accY.setText(String.valueOf(event.values[1]));
+            tv_accZ.setText(String.valueOf(event.values[2]));
+        }
+
+        else if (sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            tv_gyrX.setText(String.valueOf(event.values[0]));
+            tv_gyrY.setText(String.valueOf(event.values[1]));
+            tv_gyrZ.setText(String.valueOf(event.values[2]));
+        }
     }
 
     @Override
