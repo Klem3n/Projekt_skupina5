f63b885e9dd469d862d281ec7e7c96ce124bf0b7
Matic <matic18@protonmail.com>
2020-06-01 18:57:07 +0200

    server is now connected to mongodb cluster

--- server_v2/index.js
+++ server_v2/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
+// atlas cluster url
+const uri = "mongodb+srv://atlasAdmin:atlasadmin@cluster0-fz73a.mongodb.net/test?retryWrites=true&w=majority";
 
 const app = express()
 
@@ -11,5 +15,16 @@ const routes = require('./endpoints.js')
 app.use('/', routes)
 
 const port = process.env.PORT || 5000
+
+mongoose.connect(uri, {
+    useUnifiedTopology: true
+})
+
+mongoose.Promise = global.Promise
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'Mongodb connection error....'))
+db.once('open', () => {
+    console.log("Connection to Atlas Cluster Successful.")
+})
 
 app.listen(port, () => console.log("Server listening on port " + port))
