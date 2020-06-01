31b188b2ba184385e5df1fb12bcf647e779309e3
Matic <matic18@protonmail.com>
2020-06-01 21:36:36 +0200

    mongoose location model

---
+++ server_v2/models/locationModel.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+var locationSchema = new Schema({
+    longitude: String,
+    latitude: String,
+    speed: String,
+    address: String,
+    uuid: String
+})
+
+var LocationModel = mongoose.model('locations', locationSchema)
+module.exports = LocationModel
