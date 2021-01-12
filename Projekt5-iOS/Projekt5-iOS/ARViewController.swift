cb3f61e2e7f7042adf818f75195969fb6718e820
Matic Lakota <matic18@protonmail.com>
2021-01-12 21:22:12 +0100

    radar fetching / updating, ar view almost done

--- Projekt5-iOS/Projekt5-iOS/ARViewController.swift
+++ Projekt5-iOS/Projekt5-iOS/ARViewController.swift
@@ -12,28 +12,65 @@ import SceneKit
 import UIKit
 import CoreLocation
 import Alamofire
+import SwiftyJSON
 
 class ARViewController: UIViewController {
     
     var sceneLocationView = SceneLocationView()
- 
+    var allAnnotationNodes = [LocationAnnotationNode]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("did load ar view")
-        sceneLocationView.run()
-        view.addSubview(sceneLocationView)
         
-        // fetch current radar reports from server
-        
+        makeNetworkRequest() { (jsonResult) -> () in
+            print("network request callback here")
+            self.setupAnnotationNodes(fetchedData: jsonResult)
+            self.sceneLocationView.run()
+            self.view.addSubview(self.sceneLocationView)
+            self.sceneLocationView.addLocationNodesWithConfirmedLocation(locationNodes: self.allAnnotationNodes)
+        }
     }
+    
+    func setupAnnotationNodes(fetchedData: JSON) {
+        print("setupAnnotationNodes called")
+        
+        for element in fetchedData {
+            
+            let lat = element.1["latitude"].doubleValue
+            let lon = element.1["longitude"].doubleValue
+            
+            let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+            let location = CLLocation(coordinate: coordinate, altitude: 280)
+            
+            let annotation = UIImage(named: "police")!
+            let annotationNode = LocationAnnotationNode(location: location, image: annotation)
+            annotationNode.scaleRelativeToDistance = true
+            annotationNode.scalingScheme = .normal
+            annotationNode.continuallyUpdatePositionAndScale = true
+            
+            allAnnotationNodes.append(annotationNode)
+        }
+    }
+    
+    
+    func makeNetworkRequest(completion: @escaping (JSON) -> ()) {
+        // fetch current radar locations from server
+        AF.request("http://192.168.1.127:5000/api/v1/fetch_all_radar").validate().responseJSON { response in
+            if let data = response.data {
+                let jsonData = try! JSON(data: data)
+                completion(jsonData)
+            }
+        }
+    }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         
         let coordinate = CLLocationCoordinate2D(latitude: 46.534995, longitude: 15.664191)
         let location = CLLocation(coordinate: coordinate, altitude: 280)
-        //let view = UIView() // or a custom UIView subclass
         
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
         label.center = CGPoint(x: 160, y: 285)
@@ -41,14 +78,13 @@ class ARViewController: UIViewController {
         label.textColor = UIColor.red
         label.font = UIFont.boldSystemFont(ofSize: 100)
         label.text = "Radarska kontrola!"
-        //self.view.addSubview(label)
         
         let annotation = UIImage(named: "police")!
         let annotationNode = LocationAnnotationNode(location: location, image: annotation)
         annotationNode.scaleRelativeToDistance = true
         annotationNode.scalingScheme = .normal
         annotationNode.continuallyUpdatePositionAndScale = true
-        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: annotationNode)
+        //sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: annotationNode)
     }
     
     override func viewDidLayoutSubviews() {
